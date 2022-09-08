7716b9cde1c5772fc29c21b51d6ab8920bdb5eb3
Hotcers <venezuelavvv01@protonmail.com>
2022-09-07 22:04:34 -0500

    first commit

---
+++ src/PositiveNegative.js
@@ -0,0 +1,11 @@
+function num(param) {
+    if (param<0) {
+        console.log("Is negative");
+    }else if(param>0){
+        console.log("Is positive");
+    } else {
+        console.log("Is zero");
+    }
+
+}
+console.log(num(0));
